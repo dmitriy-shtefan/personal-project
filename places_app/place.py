97b7c5c2bbcbd69cf6eb718a8fb7efcdd3ce72bd
Dmitriy Shtefan <dmitriy.shtefan@gameloft.com>
2026-08-02 16:02:22 +0300

    Commit bug

--- places_app/place.py
+++ places_app/place.py
@@ -21,8 +21,8 @@ class Place:
 
     def to_html(self):
         return f"""
-            <article class="place-card">
-                <img class="place-image" 
+            <article class="places-card">
+                <img class="places-image" 
                      src="{self.image_url}" 
                      alt="{self.name}" />
                 
